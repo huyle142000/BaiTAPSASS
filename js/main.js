83e84bd6ad87b58e1e8019db288894933d5aafc6
huy142000 <lehaminhhuy.93@gmail.com>
2022-08-20 18:34:25 +0700

    daycode

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// header Nav
+var headerNav = document.getElementById('header_tabula');
+window.addEventListener('scroll', function () {
+    let scrollTops = document.getElementById('scrollTops');
+    console.log(scrollTops.getBoundingClientRect().top)
+    if (Math.floor(scrollTops.getBoundingClientRect().top) <= 0) {
+        headerNav.classList.add('show')
+    }else{
+        headerNav.classList.remove('show')
+
+    }
+})
+// optionShow
+var tabulaOptionShow = document.querySelector('#tabula_optionShow');
+tabulaOptionShow.addEventListener('click', function (e) {
+    document.querySelector('.tabula_options').classList.toggle('show')
+
+})
+//SUBNAV
+var showNavSub = document.querySelectorAll('.showNavSub')
+for (let i = 0; i < showNavSub.length; i++) {
+    showNavSub[i].addEventListener('click', function (e) {
+        showNavSub[i].parentElement.parentElement.classList.toggle('show');
+    });
+}
+var faUp = document.querySelectorAll('.fa-angle-up')
+for (let i = 0; i < faUp.length; i++) {
+    faUp[i].addEventListener('click', function (e) {
+        faUp[i].parentElement.parentElement.classList.toggle('show');
+    });
+}
+//navIcon
+var navbarToggler = document.querySelector('.navbar-toggler')
+navbarToggler.addEventListener('click', function (e) {
+    document.querySelector('body').classList.toggle('show');
+});
+//Home Hover
+let tabulaSubHome = document.querySelector('.tabula_sub-home .tabula_sub')
+window.addEventListener('resize', function (e) {
+    let valueResize = 1280 - document.querySelector('body').clientWidth;
+    var leftHome = tabulaSubHome.style.left
+    tabulaSubHome.style.left = `${-375 + valueResize}px`
+    console.log(leftHome)
+});
+let valueResize = 1280 - document.querySelector('body').clientWidth;
+var leftHome = tabulaSubHome.style.left
+// selectEmail
+let selectEmail = document.querySelectorAll('.tabula_select li');
+for (let i = 0; i < selectEmail.length; i++) {
+    selectEmail[i].addEventListener('click', function (e) {
+        document.querySelector('#tabula_optionShow').innerHTML = selectEmail[i].innerHTML;
+        document.querySelector('.tabula_options').classList.remove('show');
+    })
+}
